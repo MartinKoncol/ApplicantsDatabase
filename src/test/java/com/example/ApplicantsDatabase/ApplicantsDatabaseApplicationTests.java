a5e628a37d4951f9ef7cff27489fa0064d08fb05
Martin Končol <martin.koncol@seznam.cz>
2023-12-10 18:16:56 +0100

    feat: Initial commit

---
+++ src/test/java/com/example/ApplicantsDatabase/ApplicantsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplicantsDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
